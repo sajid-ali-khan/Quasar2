f5a05985b49f9e60f44afca7cefab7b204155365
sajid@windows <sajid@windows>
2025-03-09 10:28:45 +0530

    added resources

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from routes.quiz_routes import quiz_bp
+from routes.resource_routes import res_bp
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ CORS(app)
 
 # Register Blueprints (modular routes)
 app.register_blueprint(quiz_bp)
+app.register_blueprint(res_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -2,4 +2,7 @@ import os
 
 class Config:
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyC5V6snaTxR0tJo8g-J5xELW7kSO7Jm2hQ")
+    API_KEY = "AIzaSyCtHMiP9FGEGpk5VHTE1RZxuJGvafdLf0Y"  # Replace with your API Key
+    SEARCH_ENGINE_ID = "1513b5c10fdc84abd"  # Replace with your Search Engine ID
+    YOUTUBE_API_KEY = "AIzaSyCvCFu0t5kG7iOnZ_cVn2BKs2Awi470Uig"
 
